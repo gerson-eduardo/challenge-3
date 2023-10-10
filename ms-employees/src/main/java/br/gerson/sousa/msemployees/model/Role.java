1c2eaf71d7d2977b6fa3ae496f1386e4e7f7d54f
gerson <gerson@email.com>
2023-10-10 15:01:13 -0300

    feat: created the Role entity class

---
+++ ms-employees/src/main/java/br/gerson/sousa/msemployees/model/Role.java
@@ -0,0 +1,20 @@
+package br.gerson.sousa.msemployees.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String role;
+}
