f283f3222f24974e70adb950df02554af6c1c90f
gerson <gerson@email.com>
2023-10-10 17:13:10 -0300

    feat: added some CRUD methods for the EmployeeRepository interface

--- ms-employees/src/main/java/br/gerson/sousa/msemployees/repository/EmployeeRepository.java
+++ ms-employees/src/main/java/br/gerson/sousa/msemployees/repository/EmployeeRepository.java
@@ -4,9 +4,14 @@ import br.gerson.sousa.msemployees.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Employee save(Employee employee);
+    List<Employee> findAll();
     Optional<Employee> findById(Long id);
+    Optional<Employee> findByCpf(String cpf);
+    Optional<Employee> findByEmail(String email);
 }
