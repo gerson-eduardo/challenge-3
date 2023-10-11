1ed7d12e2943dcad985c6d5c699ed0f6cf1e5482
gerson <gerson@email.com>
2023-10-11 17:33:50 -0300

    feat: created the deleteById method inside the RoleController

--- ms-employees/src/main/java/br/gerson/sousa/msemployees/controller/RoleController.java
+++ ms-employees/src/main/java/br/gerson/sousa/msemployees/controller/RoleController.java
@@ -43,4 +43,10 @@ public class RoleController {
     public ResponseEntity<Role> findByCpf(@PathVariable String cpf){
         return ResponseEntity.status(HttpStatus.FOUND).body(service.findByEmployee(cpf).get());
     }
+
+    @DeleteMapping("/role/id/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable Long id){
+        service.deleteById(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Role deleted successfully");
+    }
 }
