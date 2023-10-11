412cf312bb71cc67dfd3cefce4f089b4b99e854a
gerson <gerson@email.com>
2023-10-11 17:22:47 -0300

    feat: added the deleteById and deleteByCpf methods to the EmployeeRepository

--- ms-employees/src/main/java/br/gerson/sousa/msemployees/repository/EmployeeRepository.java
+++ ms-employees/src/main/java/br/gerson/sousa/msemployees/repository/EmployeeRepository.java
@@ -14,4 +14,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findById(Long id);
     Optional<Employee> findByCpf(String cpf);
     Optional<Employee> findByEmail(String email);
+    void deleteById(Long id);
+    void deleteByCpf(String cpf);
 }
