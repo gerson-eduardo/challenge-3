010626e62389358c324665938b16dba41e4d25fc
gerson <gerson@email.com>
2023-10-13 13:44:11 -0300

    feat: created the ms-voting project structure

---
+++ ms-voting/src/main/java/br/gerson/sousa/msvoting/MsVotingApplication.java
@@ -0,0 +1,13 @@
+package br.gerson.sousa.msvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVotingApplication.class, args);
+	}
+
+}
