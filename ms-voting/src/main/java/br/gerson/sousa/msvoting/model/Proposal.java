23f26e9d7f0f86b9e76fa51d9778a4c81a8f028a
gerson <gerson@email.com>
2023-10-17 17:42:22 -0300

    refactor: updated the Proposal class to use lombok

--- ms-voting/src/main/java/br/gerson/sousa/msvoting/model/Proposal.java
+++ ms-voting/src/main/java/br/gerson/sousa/msvoting/model/Proposal.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Proposal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,70 +22,11 @@ public class Proposal {
     private String endingDate;
     private boolean result;
 
-    public Proposal() {}
-
     public Proposal(String name, String description, String creationDate, String endingDate, boolean result) {
         this.name = name;
         this.description = description;
         this.creationDate = creationDate;
         this.endingDate = endingDate;
-        this.result = result;
-    }
-
-    public Proposal(Long id, String name, String description, String creationDate, String endingDate, boolean result) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.creationDate = creationDate;
-        this.endingDate = endingDate;
-        this.result = result;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(String creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getEndingDate() {
-        return endingDate;
-    }
-
-    public void setEndingDate(String endingDate) {
-        this.endingDate = endingDate;
-    }
-
-    public boolean isResult() {
-        return result;
-    }
-
-    public void setResult(boolean result) {
         this.result = result;
     }
 }
