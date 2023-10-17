473b228a479cff31b2273ebd5c0c426e11ff5d5d
gerson <gerson@email.com>
2023-10-17 15:47:28 -0300

    feat: created a simple implementation of the save method inside the VoteController

--- ms-voting/src/main/java/br/gerson/sousa/msvoting/controller/VoteController.java
+++ ms-voting/src/main/java/br/gerson/sousa/msvoting/controller/VoteController.java
@@ -1,7 +1,12 @@
 package br.gerson.sousa.msvoting.controller;
 
+import br.gerson.sousa.msvoting.model.Vote;
 import br.gerson.sousa.msvoting.service.VoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,11 @@ public class VoteController {
     @Autowired
     public VoteController(VoteService service){
         this.service = service;
+    }
+
+    @PostMapping("/vote")
+    public ResponseEntity<String> create(@RequestBody Vote vote){
+        service.save(vote);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Created");
     }
 }
