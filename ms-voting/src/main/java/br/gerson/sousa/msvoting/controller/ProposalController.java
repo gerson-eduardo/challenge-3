e3261d720e16e446f295c0ab2cde964a29a73c34
gerson <gerson@email.com>
2023-10-17 15:35:21 -0300

    feat: created a simple implementation of the save proposal method inside the controller

--- ms-voting/src/main/java/br/gerson/sousa/msvoting/controller/ProposalController.java
+++ ms-voting/src/main/java/br/gerson/sousa/msvoting/controller/ProposalController.java
@@ -1,7 +1,13 @@
 package br.gerson.sousa.msvoting.controller;
 
+import br.gerson.sousa.msvoting.dto.SaveProposalDto;
+import br.gerson.sousa.msvoting.model.Proposal;
 import br.gerson.sousa.msvoting.service.ProposalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +20,12 @@ public class ProposalController {
     public ProposalController(ProposalService service){
         this.service = service;
     }
+
+    @PostMapping("/proposal")
+    public ResponseEntity<String> create(@RequestBody SaveProposalDto dto){
+        service.save(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Created");
+    }
+
+
 }
