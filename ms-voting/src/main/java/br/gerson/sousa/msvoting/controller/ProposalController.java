8a22cc44983c65ca43580f6a63b94c12874eb474
gerson <gerson@email.com>
2023-10-17 15:37:21 -0300

    feat: created a simple implementation of the findAll method inside the ProposalController

--- ms-voting/src/main/java/br/gerson/sousa/msvoting/controller/ProposalController.java
+++ ms-voting/src/main/java/br/gerson/sousa/msvoting/controller/ProposalController.java
@@ -6,10 +6,9 @@ import br.gerson.sousa.msvoting.service.ProposalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,6 +24,11 @@ public class ProposalController {
     public ResponseEntity<String> create(@RequestBody SaveProposalDto dto){
         service.save(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body("Created");
+    }
+
+    @GetMapping("/proposal")
+    public ResponseEntity<List<SaveProposalDto>> findAll(){
+        return ResponseEntity.ok().body(service.findALl());
     }
 
 
