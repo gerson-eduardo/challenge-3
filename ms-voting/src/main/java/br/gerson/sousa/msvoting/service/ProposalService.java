ee67d0df9069fb4d2371e47b687fdf8df33502d2
gerson <gerson@email.com>
2023-10-17 14:59:25 -0300

    refactor: updated the name of the SaveProposalDto inside the ProposalService

--- ms-voting/src/main/java/br/gerson/sousa/msvoting/service/ProposalService.java
+++ ms-voting/src/main/java/br/gerson/sousa/msvoting/service/ProposalService.java
@@ -1,6 +1,6 @@
 package br.gerson.sousa.msvoting.service;
 
-import br.gerson.sousa.msvoting.dto.ProposalDto;
+import br.gerson.sousa.msvoting.dto.SaveProposalDto;
 import br.gerson.sousa.msvoting.model.Proposal;
 import br.gerson.sousa.msvoting.repository.ProposalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProposalService {
@@ -20,20 +19,20 @@ public class ProposalService {
         this.repository = repository;
     }
 
-    public void save(ProposalDto dto){
+    public void save(SaveProposalDto dto){
         repository.save(dto.toModel());
     }
 
-    public List<ProposalDto> findALl(){
-        List<ProposalDto> dtos = new ArrayList<>();
+    public List<SaveProposalDto> findALl(){
+        List<SaveProposalDto> dtos = new ArrayList<>();
         for(Proposal proposal: repository.findAll()){
-            dtos.add(new ProposalDto(proposal));
+            dtos.add(new SaveProposalDto(proposal));
         }
         return dtos;
     }
 
-    public ProposalDto findById(Long id){
-        return new ProposalDto(repository.findById(id).get());
+    public SaveProposalDto findById(Long id){
+        return new SaveProposalDto(repository.findById(id).get());
     }
 
     public void deleteById(Long id){
