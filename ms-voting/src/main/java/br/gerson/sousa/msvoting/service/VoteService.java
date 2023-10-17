59b98118e459e8000430f9b796d712a202990bf3
gerson <gerson@email.com>
2023-10-17 15:21:06 -0300

    feat: added simple implementations of the VoteRepository inside the VoteService

--- ms-voting/src/main/java/br/gerson/sousa/msvoting/service/VoteService.java
+++ ms-voting/src/main/java/br/gerson/sousa/msvoting/service/VoteService.java
@@ -1,8 +1,11 @@
 package br.gerson.sousa.msvoting.service;
 
+import br.gerson.sousa.msvoting.model.Vote;
 import br.gerson.sousa.msvoting.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VoteService {
@@ -13,5 +16,23 @@ public class VoteService {
         this.repository = repository;
     }
 
+    public void save(Vote vote){
+        repository.save(vote);
+    }
 
+    public List<Vote> findAll(){
+        return repository.findAll();
+    }
+
+    public List<Vote> findAllByProposal_Name(String name){
+        return repository.findAllByProposal_Name(name);
+    }
+
+    public List<Vote> findAllByProposal_Id(Long id){
+        return repository.findAllByProposal_Id(id);
+    }
+
+    public List<Vote> findAllByCpf(String cpf){
+        return repository.findAllByCpf(cpf);
+    }
 }
