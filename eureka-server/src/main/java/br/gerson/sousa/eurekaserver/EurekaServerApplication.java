5caffa2d5412e7969a96a57b7d41769e80925aeb
gerson <gerson@email.com>
2023-10-18 18:24:07 -0300

    feat: created the eureka-server project structure

---
+++ eureka-server/src/main/java/br/gerson/sousa/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package br.gerson.sousa.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
