107b9edd982d6c6e6db85fcdd83756fd03f9c0b8
gerson <gerson@email.com>
2023-10-18 18:20:18 -0300

    feat: created routes to the ms-employees and ms-voting microservices inside the api-gateway

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,22 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
+    @Bean
+    protected RouteLocator routes(RouteLocatorBuilder builder){
+        return builder
+                .routes()
+                .route(r ->  r.path("/ms-employees/**").uri("lb://ms-employees"))
+                .route(r -> r.path("/ms-voting/**").uri("lb://ms-voting"))
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
