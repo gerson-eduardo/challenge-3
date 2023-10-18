d828d48cac3adaaac08a34f97a2b302427787e93
gerson <gerson@email.com>
2023-10-18 14:53:05 -0300

    feat: created a constructor inside the FindProposalDto that uses a Proposal as argument

--- ms-voting/src/main/java/br/gerson/sousa/msvoting/dto/FindProposalDto.java
+++ ms-voting/src/main/java/br/gerson/sousa/msvoting/dto/FindProposalDto.java
@@ -1,5 +1,6 @@
 package br.gerson.sousa.msvoting.dto;
 
+import br.gerson.sousa.msvoting.model.Proposal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,12 @@ public class FindProposalDto {
     private String creationDate;
     private String endingDate;
     private Boolean approved;
+
+    public FindProposalDto(Proposal proposal){
+        this.name = proposal.getName();
+        this.description = proposal.getDescription();
+        this.creationDate = proposal.getCreationDate();
+        this.endingDate = proposal.getEndingDate();
+        this.approved = proposal.getApproved();
+    }
 }
