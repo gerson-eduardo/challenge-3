13e805157e3a27c88022b5ca982516d997b15549
gerson <gerson@email.com>
2023-10-19 17:31:41 -0300

    feat: added the url and fixed the return type of the RoleFeignClient method

--- ms-voting/src/main/java/br/gerson/sousa/msvoting/feignCLient/RoleFeignClient.java
+++ ms-voting/src/main/java/br/gerson/sousa/msvoting/feignCLient/RoleFeignClient.java
@@ -1,13 +1,12 @@
 package br.gerson.sousa.msvoting.feignCLient;
 
-import br.gerson.sousa.msvoting.dto.RoleDto;
+import br.gerson.sousa.msvoting.dto.FindRoleDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-employees")
+@FeignClient(name = "ms-employees", url = "http://localhost:8080/ms-employees")
 public interface RoleFeignClient {
-    @GetMapping("/ms-employees/api/v1/role/employee/{cpf}")
-    ResponseEntity<RoleDto> findByCpf(@PathVariable String cpf);
+    @GetMapping("/api/v1/role/employee/{cpf}")
+    FindRoleDto findByCpf(@PathVariable String cpf);
 }
