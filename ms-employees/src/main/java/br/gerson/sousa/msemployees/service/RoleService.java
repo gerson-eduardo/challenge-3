b417d4408ece527079dc22ecf61441bdaa091b35
gerson <gerson@email.com>
2023-10-19 11:28:35 -0300

    feat: added exception handling to the role service methods

--- ms-employees/src/main/java/br/gerson/sousa/msemployees/service/RoleService.java
+++ ms-employees/src/main/java/br/gerson/sousa/msemployees/service/RoleService.java
@@ -2,12 +2,14 @@ package br.gerson.sousa.msemployees.service;
 
 import br.gerson.sousa.msemployees.dto.FindRoleDto;
 import br.gerson.sousa.msemployees.dto.SaveRoleDto;
+import br.gerson.sousa.msemployees.ex.EntityNotFoundException;
 import br.gerson.sousa.msemployees.model.Employee;
 import br.gerson.sousa.msemployees.model.Role;
 import br.gerson.sousa.msemployees.repository.EmployeeRepository;
 import br.gerson.sousa.msemployees.repository.RoleRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -50,12 +52,20 @@ public class RoleService {
         return dtos;
     }
 
-    public Optional<Role> findById(Long id){
-        return roleRepository.findById(id);
+    public FindRoleDto findById(Long id){
+        Optional<Role> role = roleRepository.findById(id);
+        if(role.isEmpty()){
+            throw new EntityNotFoundException("Role with id " + id + " not found!");
+        }
+        return new FindRoleDto(role.get());
     }
 
-    public Optional<Role> findByCpf(String cpf){
-        return roleRepository.findByEmployee_Cpf(cpf);
+    public FindRoleDto findByCpf(String cpf){
+        Optional<Role> role = roleRepository.findByEmployee_Cpf(cpf);
+        if(role.isEmpty()){
+            throw new EntityNotFoundException("Role with employee cpf " + cpf + " not found!");
+        }
+        return new FindRoleDto(role.get());
     }
 
     @Transactional
@@ -74,11 +84,19 @@ public class RoleService {
 
     @Transactional
     public void deleteById(Long id){
-        roleRepository.deleteById(id);
+        try {
+            roleRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new EntityNotFoundException("Role with id " + id + " not found!");
+        }
     }
 
     @Transactional
     public void deleteByCpf(String cpf){
-        roleRepository.deleteByEmployee_Cpf(cpf);
+        try {
+            roleRepository.deleteByEmployee_Cpf(cpf);
+        }catch(EmptyResultDataAccessException e){
+            throw new EntityNotFoundException("Role with cpf " + cpf + " not found!");
+        }
     }
 }
