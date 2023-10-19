4ac119b540645505fd8ee5c397b87c2e0d76d335
gerson <gerson@email.com>
2023-10-18 22:45:11 -0300

    test: created the EmployeeServiceTest class

---
+++ ms-employees/src/test/java/br/gerson/sousa/msemployees/service/EmployeeServiceTest.java
@@ -0,0 +1,40 @@
+package br.gerson.sousa.msemployees.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findByCpf() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void deleteByCpf() {
+    }
+}
