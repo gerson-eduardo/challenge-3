acb0891619661e6b40a10a7d07800f37daff31a0
gerson <gerson@email.com>
2023-10-19 08:47:09 -0300

    test: created the RoleService test class

---
+++ ms-employees/src/test/java/br/gerson/sousa/msemployees/service/RoleServiceTest.java
@@ -0,0 +1,36 @@
+package br.gerson.sousa.msemployees.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByCpf() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void deleteByCpf() {
+    }
+}
