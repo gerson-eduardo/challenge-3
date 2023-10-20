7d542cd4e1c1bb7496e7fbc7ed9f7c486666a6a0
gerson <gerson@email.com>
2023-10-20 10:01:20 -0300

    feat: added jpa annotations to the Proposal entity class

--- ms-voting/src/main/java/br/gerson/sousa/msvoting/model/Proposal.java
+++ ms-voting/src/main/java/br/gerson/sousa/msvoting/model/Proposal.java
@@ -1,9 +1,6 @@
 package br.gerson.sousa.msvoting.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +13,9 @@ public class Proposal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String name;
+    @Column(nullable = false)
     private String description;
     private String creationDate;
     private String endingDate;
