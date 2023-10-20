b2695f1c289ac380f51fbd5b3198898bf7a1447e
gerson <gerson@email.com>
2023-10-20 13:09:23 -0300

    feat: create static SaveRoleDto and FindRoleDto objects in the RoleCOnstants class

--- ms-employees/src/test/java/br/gerson/sousa/msemployees/common/RoleConstants.java
+++ ms-employees/src/test/java/br/gerson/sousa/msemployees/common/RoleConstants.java
@@ -1,5 +1,7 @@
 package br.gerson.sousa.msemployees.common;
 
+import br.gerson.sousa.msemployees.dto.FindRoleDto;
+import br.gerson.sousa.msemployees.dto.SaveRoleDto;
 import br.gerson.sousa.msemployees.model.Role;
 
 import java.util.Arrays;
@@ -10,6 +12,8 @@ import static br.gerson.sousa.msemployees.common.EmployeeConstants.EMPLOYEE;
 public class RoleConstants {
     public static final Role ROLE = new Role(1L, EMPLOYEE, "ADMIN");
     public static final Role ROLE2 = new Role(2L, EMPLOYEE, "USER");
+    public static final SaveRoleDto S_ROLE_DTO = new SaveRoleDto(EMPLOYEE.getCpf(), "USER");
+    public static final FindRoleDto F_ROLE_DTO = new FindRoleDto(ROLE);
     public static final List<Role> ROLE_LIST = Arrays.asList(ROLE, ROLE2);
 
 }
