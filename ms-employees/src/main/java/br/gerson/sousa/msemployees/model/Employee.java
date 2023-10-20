b5c41a738e8736221e128f9da68b9fd59b0482ff
gerson <gerson@email.com>
2023-10-20 09:56:39 -0300

    feat: added jpa annotations to the Employee entity class

--- ms-employees/src/main/java/br/gerson/sousa/msemployees/model/Employee.java
+++ ms-employees/src/main/java/br/gerson/sousa/msemployees/model/Employee.java
@@ -1,9 +1,6 @@
 package br.gerson.sousa.msemployees.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,9 +14,13 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true, nullable = false)
     private String cpf;
+    @Column(nullable = false)
     private String address;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
 
     public Employee(String name, String cpf, String address, String email, String password) {
