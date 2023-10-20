c06845f8960747dd64ece1dec01a377f9b414529
gerson <gerson@email.com>
2023-10-20 13:47:21 -0300

    test: created teh EmployeeControllerTest class

---
+++ ms-employees/src/test/java/br/gerson/sousa/msemployees/controller/EmployeeControllerTest.java
@@ -0,0 +1,40 @@
+package br.gerson.sousa.msemployees.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByid() {
+    }
+
+    @Test
+    void findByCpf() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteByCpf() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
