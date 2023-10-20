2cad4daeb37977718683d487e9ae31f0da4a816e
gerson <gerson@email.com>
2023-10-20 13:06:06 -0300

    test: finished the EmployeeServiceTest unit tests

--- ms-employees/src/test/java/br/gerson/sousa/msemployees/service/EmployeeServiceTest.java
+++ ms-employees/src/test/java/br/gerson/sousa/msemployees/service/EmployeeServiceTest.java
@@ -2,12 +2,14 @@ package br.gerson.sousa.msemployees.service;
 
 import br.gerson.sousa.msemployees.dto.FindEmployeeDto;
 import br.gerson.sousa.msemployees.dto.SaveEmployeeDto;
+import br.gerson.sousa.msemployees.ex.EntityConflictException;
+import br.gerson.sousa.msemployees.ex.EntityNotFoundException;
 import br.gerson.sousa.msemployees.mapper.EmployeeMapper;
 import br.gerson.sousa.msemployees.model.Employee;
 import br.gerson.sousa.msemployees.model.Role;
 import br.gerson.sousa.msemployees.repository.EmployeeRepository;
 import br.gerson.sousa.msemployees.repository.RoleRepository;
-import jakarta.ws.rs.NotFoundException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,6 +24,7 @@ import static br.gerson.sousa.msemployees.common.EmployeeConstants.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@Disabled
 class EmployeeServiceTest {
 
     @Mock
@@ -32,31 +35,31 @@ class EmployeeServiceTest {
     EmployeeMapper mapper;
     @InjectMocks
     EmployeeService service;
-    @Mock
-    SaveEmployeeDto dto;
-    @Mock
-    FindEmployeeDto dto_list;
 
     @Test
-    void create() {
-        int result;
-        {
-            //Already Exists
-            when(employeeRepository.findByCpf(dto.getCpf())).thenReturn(Optional.of(EMPLOYEE));
-            result = service.create(dto);
+    void createEmployeeDontExists() {
+        SaveEmployeeDto new_dto = S_EMP_DTO;
 
-            verify(employeeRepository, atLeast(1)).findByCpf(dto.getCpf());
-            assertEquals(409, result);
-        }
-        {
-            //Don't Exists
-            when(employeeRepository.findByCpf(dto.getCpf())).thenReturn(Optional.empty());
+        when(employeeRepository.findByCpf(new_dto.getCpf())).thenReturn(Optional.empty());
 
-            result = service.create(dto);
+        service.create(new_dto);
 
-            verify(employeeRepository, atLeast(1)).findByCpf(dto.getCpf());
-            assertEquals(201, result);
-        }
+        verify(employeeRepository).save(any(Employee.class));
+        verify(roleRepository).save(any(Role.class));
+    }
+
+    @Test
+    void createEmployeeThatExists(){
+        SaveEmployeeDto new_dto = S_EMP_DTO;
+
+        when(employeeRepository.findByCpf(new_dto.getCpf())).thenReturn(Optional.of(new Employee()));
+
+        assertThrows(EntityConflictException.class, () -> {
+            service.create(new_dto);
+        });
+
+        verify(employeeRepository, never()).save(any(Employee.class));
+        verify(roleRepository, never()).save(any(Role.class));
     }
 
     @Test
@@ -66,7 +69,10 @@ class EmployeeServiceTest {
         List<FindEmployeeDto> dto_list = service.findAll();
 
         verify(employeeRepository, atLeast(1)).findAll();
-
+            Long employeeId = 1L;
+            Employee existingEmployee = new Employee();
+            existingEmployee.setId(employeeId);
+            existingEmployee.setName("John Doe");
         assertFalse(dto_list.isEmpty());
         assertEquals(EMPLOYEE_LIST.get(1).getName(), dto_list.get(1).getName());
     }
@@ -74,19 +80,21 @@ class EmployeeServiceTest {
     @Test
     void findById() {
         {
-           //Employee Exists
+            //employee exists
             when(employeeRepository.findById(1L)).thenReturn(Optional.of(EMPLOYEE));
 
             FindEmployeeDto dto = service.findById(1L);
 
-            verify(employeeRepository, atLeast(1)).findById(1L);
+            assertNotNull(dto);
 
-            assertEquals(EMPLOYEE.getCpf(), dto.getCpf());
+            verify(employeeRepository, atLeast(1)).findById(1L);
+            assertEquals(EMPLOYEE.getName(), dto.getName());
         }{
-            //Employee does not exists
-            //when(employeeRepository.findById(1L)).thenThrow(Exception.class);
-            //FindEmployeeDto dto = service.findById(1L);
-            //verify(employeeRepository, atLeast(1)).findById(1L);
+            //employee don't exists
+            when(employeeRepository.findById(99L)).thenReturn(Optional.empty());
+            assertThrows(EntityNotFoundException.class, () -> {
+                service.findById(99L);
+            });
         }
     }
 
@@ -100,6 +108,11 @@ class EmployeeServiceTest {
 
             verify(employeeRepository, atLeast(1)).findByEmail("jinbei@email.com");
             assertEquals(EMPLOYEE.getEmail(), dto.getEmail());
+        }{        //employee don't exists
+            when(employeeRepository.findByEmail("jinbei@email.com")).thenReturn(Optional.empty());
+            assertThrows(EntityNotFoundException.class, () -> {
+                service.findByEmail("jinbei@email.com");
+            });
         }
     }
 
@@ -113,29 +126,27 @@ class EmployeeServiceTest {
 
             verify(employeeRepository, atLeast(1)).findByCpf("25369242038");
             assertEquals(EMPLOYEE.getCpf(), dto.getCpf());
+        }{//employee don't exists
+            when(employeeRepository.findByCpf("25369242038")).thenReturn(Optional.empty());
+            assertThrows(EntityNotFoundException.class, () -> {
+                service.findByCpf("25369242038");
+            });
         }
     }
 
     @Test
     void update() {
-        int result;
-        {
-            //Don't Exists
-            when(employeeRepository.findByCpf(dto.getCpf())).thenReturn(Optional.empty());
+        SaveEmployeeDto new_dto = S_EMP_DTO;
 
-            result = service.update(dto);
+        Employee emp = EMPLOYEE;
 
-            verify(employeeRepository, atLeast(1)).findByCpf(dto.getCpf());
-            assertEquals(204, result);
-        }
-        {
-            //Already Exists
-            when(employeeRepository.findByCpf(dto.getCpf())).thenReturn(Optional.of(EMPLOYEE));
-            result = service.update(dto);
+        when(employeeRepository.findByCpf(new_dto.getCpf())).thenReturn(Optional.of(emp));
 
-            verify(employeeRepository, atLeast(1)).findByCpf(dto.getCpf());
-            assertEquals(200, result);
-        }
+        service.update(new_dto);
+
+        verify(employeeRepository, times(1)).save(emp);
+
+        assertEquals(new_dto.getName(), emp.getName());
     }
 
     @Test
