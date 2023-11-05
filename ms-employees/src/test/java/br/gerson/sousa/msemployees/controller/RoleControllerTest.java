94313c1ad861edf6f7bfdb5bd543997b3ca6cd6f
gerson <gerson@email.com>
2023-11-04 21:54:41 -0300

    test:tested the create method with valid dto

--- ms-employees/src/test/java/br/gerson/sousa/msemployees/controller/RoleControllerTest.java
+++ ms-employees/src/test/java/br/gerson/sousa/msemployees/controller/RoleControllerTest.java
@@ -1,15 +1,21 @@
 package br.gerson.sousa.msemployees.controller;
 
+import br.gerson.sousa.msemployees.dto.SaveRoleDto;
 import br.gerson.sousa.msemployees.service.RoleService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static br.gerson.sousa.msemployees.common.RoleConstants.*;
 
 @WebMvcTest(RoleController.class)
 class RoleControllerTest {
@@ -18,12 +24,20 @@ class RoleControllerTest {
     private MockMvc mvc;
 
     @MockBean
-    private RoleService roleService;
+    private RoleService service;
 
-    private ObjectMapper mapper;
+    private ObjectMapper mapper = new ObjectMapper();
 
     @Test
-    void create() {
+    void create() throws Exception {
+        SaveRoleDto dto = S_ROLE_DTO;
+
+        Mockito.doNothing().when(service).create(dto);
+
+        mvc.perform(MockMvcRequestBuilders.post("/api/v1/role")
+                .content(objectToJson(dto))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
     }
 
     @Test
