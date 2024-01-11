33e18698fa0615be6318871d493221404f63cc29
Gerson <gerson@email.com>
2024-01-11 13:57:11 -0300

    feat: added the Api information inside the main method

--- ms-employees/src/main/java/br/gerson/sousa/msemployees/MsEmployeesApplication.java
+++ ms-employees/src/main/java/br/gerson/sousa/msemployees/MsEmployeesApplication.java
@@ -1,9 +1,12 @@
 package br.gerson.sousa.msemployees;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "MS-Employees", version = "1.2", description = "Api that stores employee and role information"))
 public class MsEmployeesApplication {
 
     public static void main(String[] args) {
