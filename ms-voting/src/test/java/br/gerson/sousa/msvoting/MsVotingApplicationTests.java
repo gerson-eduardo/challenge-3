e6d541a92fa479c940ef20fad76cea3b423bdc1b
Gerson <gerson@email.com>
2024-01-15 19:52:58 -0300

    feat: added the API info to the SwaggerUi OpenAPIDefinition annotation

--- ms-voting/src/test/java/br/gerson/sousa/msvoting/MsVotingApplicationTests.java
+++ ms-voting/src/test/java/br/gerson/sousa/msvoting/MsVotingApplicationTests.java
@@ -1,9 +1,12 @@
 package br.gerson.sousa.msvoting;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@OpenAPIDefinition(info = @Info(title = "MS-Voting", version = "1.2", description = "Api that stores proposal and voting information"))
 class MsVotingApplicationTests {
 
 	@Test
